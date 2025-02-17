ff862b76b7d071ef628501202a4ba501d4a4492e
Ethan L. Mines <elmines@users.noreply.github.com>
2025-02-17 16:57:38 -0500

    Add cli args to make_t5_vocab

--- make_t5_vocab.py
+++ make_t5_vocab.py
@@ -1,117 +1,102 @@
-# %%
 # STL
-import time
+import argparse
+import sys
 import json
 # 3rd Party
 from tqdm import tqdm
 from transformers import T5Tokenizer
 from datasets import load_dataset
+# Local
+from unsupamr.constants import EUROPARL_URI, T5_SEP, DEFAULT_SEQ_MODEL
 
-# %%
-# Constants
-DATASET_URI = "Helsinki-NLP/europarl"
-DATASET_SUBSETS = ["de-en", "en-es", "de-es"]
-# They don't use a real underscore character, but something else that looks like an underscore
-T5_SEP = (b'\xe2\x96\x81').decode()
+def main(raw_args=None):
+    DATASET_SUBSETS = ["de-en", "en-es", "de-es"]
 
-# %%
-# Turn these into command line arguments
-model_name = "google/mt5-small"
-concepts_limit = 25
-args_limit = 5
-ops_limit = 10
-out_path = "vocab.json"
+    parser = argparse.ArgumentParser(description="Prunes a T5 vocabulary according to available EuroParl data")
+    parser.add_argument("--model_name", default=DEFAULT_SEQ_MODEL, metavar=DEFAULT_SEQ_MODEL, nargs=1, help="Tokenizer model")
+    parser.add_argument("--max_concepts", default=25, metavar=25, type=int, help="Maximum number of concept nodes in AMR graphs")
+    parser.add_argument("--max_args", default=5, metavar=5, type=int, help="Maximum number of :ARGx and :ARGx-of labels to support (default is :ARG0 to :ARG4)")
+    parser.add_argument("--max_ops", default=10, metavar=10, type=int, help="Maximum number of :opx labels to support (default is :op0 to :op9)")
+    parser.add_argument("-o", default="vocab.json", help="Output path")
+    args = parser.parse_args(raw_args)
 
-# %%
-tokenizer = T5Tokenizer.from_pretrained(model_name)
+    model_name = args.model_name
+    concepts_limit = args.max_concepts
+    args_limit = args.max_args
+    ops_limit = args.max_ops
+    out_path = args.o
 
-# %%
-amr_whitelist = {}
-multilingual_whitelist = {}
-to_prune = tokenizer.get_vocab()
+    tokenizer = T5Tokenizer.from_pretrained(model_name)
 
-en_ids = set()
-multiling_ids = set()
+    en_ids = set()
+    multiling_ids = set()
+    for subset in DATASET_SUBSETS:
+        ds = load_dataset(path=EUROPARL_URI, name=subset)['train']['translation']
+        ds = ds[:10]
+        # ds = list(map(lambda s: s['translation'], ds))
+        for sample in tqdm(ds, desc=f"EuroParl {subset}"):
+            if "en" in sample:
+                sample_ids = tokenizer(sample['en'])['input_ids']
+                en_ids.update(sample_ids)
+                multiling_ids.update(sample_ids)
+            if "de" in sample:
+                multiling_ids.update(tokenizer(sample['de'])['input_ids'])
+            if "es" in sample:
+                multiling_ids.update(tokenizer(sample['es'])['input_ids'])
 
-for subset in DATASET_SUBSETS:
-    ds = load_dataset(path=DATASET_URI, name=subset)['train']
-    ds = list(map(lambda s: s['translation'], ds))
-    for sample in tqdm(ds, desc=f"EuroParl {subset}"):
-        if "en" in sample:
-            sample_ids = tokenizer(sample['en'])['input_ids']
-            en_ids.update(sample_ids)
-            multiling_ids.update(sample_ids)
-        if "de" in sample:
-            multiling_ids.update(tokenizer(sample['de'])['input_ids'])
-        if "es" in sample:
-            multiling_ids.update(tokenizer(sample['es'])['input_ids'])
+    amr_symbols = []
+    amr_embeddings = []
+    vocab = tokenizer.get_vocab()
+    def get_ids(w):
+        return vocab.get(f"{T5_SEP}{w}")
 
-# %%
-len(multiling_ids)
+    amr_symbols.append(":polarity -") # Not, don't, can't, etc.
+    amr_embeddings.append(get_ids('not'))
 
-# %%
-len(en_ids)
+    amr_symbols.append(":domain")
+    amr_embeddings.append(get_ids('is'))
+    amr_symbols.append(":mod") # Inverse of domain
+    amr_embeddings.append(get_ids('is'))
 
-# %%
-len(to_prune)
+    amr_symbols.append(":poss")
+    amr_embeddings.append(get_ids('his'))
+    amr_symbols.append(":poss-of")
+    amr_embeddings.append(get_ids('his'))
 
-# %%
+    # Violates original AMR notation, but we don't want this getting confused with the regular word "and" somewhere
+    amr_symbols.append("<and>")
+    amr_embeddings.append(get_ids('and'))
+    amr_symbols.append("<or>")
+    amr_embeddings.append(get_ids('or'))
+    amr_symbols.append("<contrast>")
+    amr_embeddings.append(get_ids('but'))
+    amr_symbols.append("<amr-unknown>")
+    amr_embeddings.append(get_ids('what'))
 
+    amr_symbols.extend(f"<R{i}>" for i in range(concepts_limit))
+    amr_embeddings.extend(None for _ in range(concepts_limit))
+    for i in range(args_limit):
+        amr_symbols.append(f":ARG{i}")
+        amr_embeddings.append(None)
+        amr_symbols.append(f":ARG{i}-of")
+        amr_embeddings.append(None)
 
-# %%
-amr_symbols = []
-amr_embeddings = []
+    amr_symbols.extend(f":op{i}" for i in range(ops_limit))
+    amr_embeddings.extend(None for _ in range(ops_limit))
 
-vocab = tokenizer.get_vocab()
-def get_ids(w):
-    return vocab.get(f"{T5_SEP}{w}")
+    en_ids = sorted(en_ids | {0})
+    multiling_ids = sorted(multiling_ids | {0})
 
-amr_symbols.append(":polarity -") # Not, don't, can't, etc.
-amr_embeddings.append(get_ids('not'))
+    out_dict = {
+        "pruned_english": en_ids, # Add the 0 padding token
+        "pruned_multiling": multiling_ids,
+        "amr_symbols": [{"token": w, "id": i, "embed": embed_id} for (i, (w, embed_id)) in enumerate(zip(amr_symbols, amr_embeddings), start=len(vocab))]
+    }
 
-amr_symbols.append(":domain")
-amr_embeddings.append(get_ids('is'))
-amr_symbols.append(":mod") # Inverse of domain
-amr_embeddings.append(get_ids('is'))
-
-amr_symbols.append(":poss")
-amr_embeddings.append(get_ids('his'))
-amr_symbols.append(":poss-of")
-amr_embeddings.append(get_ids('his'))
-
-# Violates original AMR notation, but we don't want this getting confused with the regular word "and" somewhere
-amr_symbols.append("<and>")
-amr_embeddings.append(get_ids('and'))
-amr_symbols.append("<or>")
-amr_embeddings.append(get_ids('or'))
-amr_symbols.append("<contrast>")
-amr_embeddings.append(get_ids('but'))
-amr_symbols.append("<amr-unknown>")
-amr_embeddings.append(get_ids('what'))
-
-amr_symbols.extend(f"<R{i}>" for i in range(concepts_limit))
-amr_embeddings.extend(None for _ in range(concepts_limit))
-for i in range(args_limit):
-    amr_symbols.append(f":ARG{i}")
-    amr_embeddings.append(None)
-    amr_symbols.append(f":ARG{i}-of")
-    amr_embeddings.append(None)
-
-amr_symbols.extend(f":op{i}" for i in range(ops_limit))
-amr_embeddings.extend(None for _ in range(ops_limit))
-
-# %%
-out_dict = {
-    "pruned_english": sorted(en_ids | {0}), # Add the 0 padding token
-    "pruned_multiling": sorted(multiling_ids | {0}),
-    "amr_symbols": [{"token": w, "id": i, "embed": embed_id} for (i, (w, embed_id)) in enumerate(zip(amr_symbols, amr_embeddings))]
-}
-
-# %%
-with open(out_path, 'w') as w:
-    json.dump(out_dict, w, indent=2)
-
-# %%
+    with open(out_path, 'w') as w:
+        json.dump(out_dict, w, indent=2)
 
 
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
