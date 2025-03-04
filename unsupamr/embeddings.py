d54141bcd1126d0296a7a2229557eae239981b03
Ethan L. Mines <elmines@users.noreply.github.com>
2025-03-03 21:00:53 -0500

    Fix bugs

--- unsupamr/embeddings.py
+++ unsupamr/embeddings.py
@@ -9,7 +9,7 @@ from .utils import VocabExt
 def expand_lm_head(head: torch.nn.Linear, vocab: VocabExt) -> torch.nn.Linear:
     
     old_num_tokens, hidden_dim = head.weight.shape
-    new_num_tokens = len(vocab)
+    new_num_tokens = old_num_tokens + len(vocab.amr_symbols)
     
     new_head = torch.nn.Linear(hidden_dim, new_num_tokens, bias=False)
     
@@ -33,10 +33,11 @@ def expand_embedding(embedding: torch.nn.Embedding, vocab: VocabExt) -> torch.nn
     new_vocab_size = old_vocab_size + len(amr_entries)
 
     new_embedding = torch.nn.Embedding(new_vocab_size, embedding_size)
-    new_embedding.weight[:old_vocab_size] = old_weight_mat
-    for entry in amr_entries:
-        if entry.embed_id is not None:
-            new_embedding.weight[entry.id] = old_weight_mat[entry.embed_id]    
+    with torch.no_grad():
+        new_embedding.weight[:old_vocab_size] = old_weight_mat
+        for entry in amr_entries:
+            if entry.embed_id is not None:
+                new_embedding.weight[entry.id] = old_weight_mat[entry.embed_id]    
     return new_embedding
 
 def mult_embedding_lookup(prob_dists: torch.Tensor, embedding: torch.nn.Embedding):
