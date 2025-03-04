66f5b8b8cf74e74e519a020f483c51dece788168
Ethan L. Mines <elmines@users.noreply.github.com>
2025-03-03 21:08:43 -0500

    Move cli instantiation

--- unsupamr/cli.py
+++ unsupamr/cli.py
@@ -1,10 +1,7 @@
 # 3rd Party
 from lightning.pytorch.cli import LightningCLI
-from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
 # Local
 from .constants import DEFAULT_SEQ_MODEL
-from .lightning_mods import TrainingMod
-from .data_mods import EuroParlDataModule
 
 class CustomCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
@@ -12,29 +9,3 @@ class CustomCLI(LightningCLI):
         parser.link_arguments("pretrained_model", "model.pretrained_model")
         parser.link_arguments("pretrained_model", "data.pretrained_model")
 
-def cli_main(**cli_kwargs):
-
-    STOPPING_METRIC = "loss"
-    model_callback = ModelCheckpoint(
-        monitor=STOPPING_METRIC,
-        mode='min',
-        filename="{epoch:02d}-{loss:.3f}"
-    )
-    early_stopping_callback = EarlyStopping(
-        monitor=STOPPING_METRIC,
-        patience=3,
-        mode='min'
-    )
-
-    return CustomCLI(
-        model_class=TrainingMod, subclass_mode_model=False,
-        datamodule_class=EuroParlDataModule, subclass_mode_data=False,
-        trainer_defaults={
-            "max_epochs": 10,
-            "callbacks": [
-                model_callback,
-                early_stopping_callback
-            ]
-        },
-        **cli_kwargs
-    )
