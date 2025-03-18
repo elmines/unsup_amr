5238e77b3c52a62cacc69ab949ba626a56f8de58
Ethan L. Mines <elmines@users.noreply.github.com>
2025-03-18 12:10:16 -0400

    Update predict CLI

--- unsupamr/predict_mods.py
+++ unsupamr/predict_mods.py
@@ -2,7 +2,7 @@
 import os
 import glob
 from itertools import starmap
-from typing import List
+from typing import List, Optional
 # 3rd Party
 import torch
 import lightning as L
@@ -16,7 +16,7 @@ from .postprocess import triple_decode, probs_to_ids
 
 class PredictMod(L.LightningModule):
     def __init__(self, 
-               version: str,
+               version: Optional[str] = None,
                pretrained_model: str = DEFAULT_SEQ_MODEL,
                temperature: float = 1.,
                max_graph_size: int = DEFAULT_MAX_GRAPH_SIZE):
@@ -32,13 +32,19 @@ class PredictMod(L.LightningModule):
         self.embeddings.eval()
         self.t2a.eval()
 
-        ckpt_pattern = os.path.join(os.path.dirname(__file__), "..", "lightning_logs", version, "checkpoints", "best-*.ckpt")
+        if version is None:
+            print("--model.version not specified. Using randomly initialized weights for the evaluation")
+            return
+
+        model_dir = os.path.join(os.path.dirname(__file__), "..", "lightning_logs", version)
+        if not os.path.exists(model_dir):
+            raise ValueError(f"Model directory {model_dir} does not exist")
+        ckpt_pattern = os.path.join(model_dir, "checkpoints", "best-*.ckpt")
         ckpt_paths = glob.glob(ckpt_pattern)
         if not ckpt_paths:
             raise ValueError(f"No checkpoint path matching {ckpt_pattern}")
         elif len(ckpt_paths) > 1:
             raise ValueError(f"Found multiple candidate checkpoint paths: {ckpt_paths}")
-
         best_checkpoint = torch.load(ckpt_paths[0])
         old_state = self.state_dict()
         matching_state_dict = {k: v for k, v in best_checkpoint['state_dict'].items() if k in old_state}
