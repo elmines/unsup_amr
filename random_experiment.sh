585f52101920e43a1ad9591ae33421e7a9671a08
Ethan L. Mines <elmines@users.noreply.github.com>
2025-03-18 14:07:09 -0400

    Clean up random_experiment pipeline

--- random_experiment.sh
+++ random_experiment.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-OUTPUT_PATH=`pwd`/random_preds_$(date +%s).txt
+TIMESTAMP=$(date +%s)
+
+prefix=`pwd`/outputs/random_$TIMESTAMP
+PREDS_OUTPUT=${prefix}_predictions.txt
+LOGGED_OUTPUT=${prefix}_output.txt
+METRICS_OUTPUT=${prefix}_metrics.txt
 
 module load conda
 conda activate unsup_amr
-python -m unsupamr.predict --output_path $OUTPUT_PATH
+python -m unsupamr.predict --output_path $PREDS_OUTPUT | tee $LOGGED_OUTPUT
 
 conda activate unsup_amr_eval
-./eval.sh $OUTPUT_PATH
+./eval.sh $PREDS_OUTPUT | tee $METRICS_OUTPUT
 
-echo Predictions available at $OUTPUT_PATH
+echo Predictions available at $PREDS_OUTPUT
+echo "Full prediction output (DFS and Penman) available at $LOGGED_OUTPUT"
+echo Metrics available at $METRICS_OUTPUT
