7d323a461d5a02924b98d3a67c084ceb30e39095
Ethan L. Mines <elmines@users.noreply.github.com>
2025-04-08 16:54:46 -0400

    Update grad_analysis

--- grad_analysis.sh
+++ grad_analysis.sh
@@ -11,6 +11,7 @@ EXTRA_ARGS=$@
 
 python -m unsupamr.fit \
     --model.log_gradients true \
+    --model.log_concept_rates true \
     --data.debug_subset true \
     --trainer.max_steps 1000 \
     --trainer.logger.version $VERSION $EXTRA_ARGS
